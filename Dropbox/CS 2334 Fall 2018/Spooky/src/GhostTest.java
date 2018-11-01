8afe6772608a0a0a8b5238d4c3475ca1818ac926
Marcos Tavarez <marcos.e.tavarez-1@ou.edu>
2018-11-01 15:42:11 -0500

    Fixed the Array List scaryKeys

---
+++ Dropbox/CS 2334 Fall 2018/Spooky/src/GhostTest.java
@@ -0,0 +1,94 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class GhostTest
+{
+    class Point
+    {
+        private Integer x;
+        private Integer y;
+        
+        public Point(Integer inX, Integer inY)
+        {
+            x = inX;
+            y = inY;
+        }
+        
+        public Integer getX()
+        {
+            return x;
+        }
+        
+        public Integer getY()
+        {
+            return y;
+        }
+        
+        public boolean equals(Point p)
+        {
+            return x.equals(p.getX()) && p.getY().equals(y);
+        }
+    }
+
+    public GhostTest()
+    {
+    }
+
+    public static void main(String[] args)
+    {
+        ArrayList<String> keys = new ArrayList<>();
+        keys.add("ghost");
+        keys.add("alien");
+        keys.add("alien");
+        keys.add("spiderman");
+        keys.add("adiabatman");
+        keys.add("scaryInversion");
+        
+        Integer[] values = {
+                new Integer(1),
+                new Integer(2),
+                new Integer(100)
+        };
+        
+        // How to fix this error?
+        //
+        ArrayList<Integer> scaryKeys = new ArrayList<Integer>(Arrays.asList(values));
+        
+        keys.forEach(k -> System.out.println(k));
+        
+        keys.removeIf(k -> k.equalsIgnoreCase("alien"));
+        
+        GhostTest gt = new GhostTest();
+        Point p1 = gt.new Point(1,1);
+        Point p2 = gt.new Point(new Integer(1), new Integer(1));
+        
+        if (p1.equals(p2))
+            System.out.println("wow");
+        else
+        {
+            System.out.println("aha");
+        }
+        TreeSet<GhostTest.Point> tp = new TreeSet<GhostTest.Point>();
+
+        ArrayList<GhostTest.Point> points = new ArrayList<>();
+        for (int index = 0; index < 5; ++index)
+        {
+            points.add(p1);
+            // How to fix this?
+            tp.add(p1);
+        }
+        
+        points.forEach(p -> System.out.println(p.getX()));
+               
+        // How to fix this?
+        // tp.addAll(points);
+        tp.forEach(p -> System.out.println(p.getX()));
+
+        
+        
+
+    }
+
+}
